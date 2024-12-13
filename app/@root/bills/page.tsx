0451c39eaa02d35284fd99e9b414ce7d7ac6775f
ThanhDat653 <datnt.653@gmail.com>
2024-12-13 19:59:48 +0700

    .

--- app/@root/bills/page.tsx
+++ app/@root/bills/page.tsx
@@ -19,21 +19,29 @@ import {
 } from "@/components/ui/pagination";
 import usePagination from "@/hooks/use-pagination";
 import { useRouter, useSearchParams } from "next/navigation";
-
-export function InputWithButton() {
-   return (
-      <div className="flex w-full max-w-sm items-center space-x-2">
-         <Input type="text" placeholder="Search by phone number" />
-         <Button type="submit">Search</Button>
-      </div>
-   );
-}
+import { cn } from "@/lib/utils";
 
 const Page = () => {
    const router = useRouter();
    const searchParams = useSearchParams();
    const initialPage = parseInt(searchParams.get("page") || "1");
+   const phoneNumber = searchParams.get("phoneNumber") || null;
    const { nextPage, prevPage, setPage, current } = usePagination(initialPage);
+
+   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const form = e.target as HTMLFormElement;
+      const params = new URLSearchParams(searchParams);
+      const inputValue = (
+         form.elements.namedItem("searchInput") as HTMLInputElement
+      )?.value;
+
+      params.set("phoneNumber", inputValue);
+      setPage(1);
+
+      router.replace(`?${params.toString()}`);
+      console.log("Search term:", current);
+   };
 
    useEffect(() => {
       const params = new URLSearchParams(searchParams);
@@ -41,27 +49,48 @@ const Page = () => {
       router.replace(`?${params.toString()}`);
    }, [current, router, searchParams]);
 
-   const { data, isLoading, error } = useSWR([current], () =>
-      searchBill(current),
+   const { data, isLoading, error } = useSWR([current, phoneNumber], () =>
+      searchBill({
+         current: current,
+         phoneNumber: phoneNumber ? parseInt(phoneNumber) : undefined,
+      }),
    );
-   console.log(current);
 
    return (
       <div className="w-full py-5 flex flex-col gap-5">
-         <InputWithButton />
+         <form
+            onSubmit={handleSearch}
+            className="flex w-full max-w-sm items-center space-x-2"
+         >
+            <Input
+               name="searchInput"
+               type="text"
+               placeholder="Search by phone number"
+            />
+            <Button type="submit">Search</Button>
+         </form>
          {isLoading && !error && <h1>...Loading</h1>}
          {data && (
-            <div className="rounded-md border">
-               <DataTable columns={BillColumns} data={data.content} />
-
-               <Pagination className="w-fit m-0">
+            <div className="flex flex-col gap-5">
+               <div className="rounded-md border">
+                  <DataTable columns={BillColumns} data={data.content} />
+               </div>
+               <Pagination className="w-fit ml-auto mr-0">
                   <PaginationContent>
                      <PaginationItem>
-                        <PaginationPrevious />
+                        <PaginationPrevious
+                           onClick={prevPage}
+                           className={cn("cursor-pointer", {
+                              "pointer-events-none": current === 1,
+                           })}
+                        />
                      </PaginationItem>
                      {Array.from({ length: data.totalPages }, (_, index) => (
                         <PaginationItem key={index}>
                            <PaginationLink
+                              className={cn("cursor-pointer", {
+                                 "pointer-events-none": current === index + 1,
+                              })}
                               isActive={index + 1 === current}
                               onClick={() => setPage(index + 1)}
                            >
@@ -71,7 +100,13 @@ const Page = () => {
                      ))}
 
                      <PaginationItem>
-                        <PaginationNext />
+                        <PaginationNext
+                           onClick={nextPage}
+                           className={cn("cursor-pointer", {
+                              "pointer-events-none":
+                                 current === data.totalPages,
+                           })}
+                        />
                      </PaginationItem>
                   </PaginationContent>
                </Pagination>
--- service/bill.ts
+++ service/bill.ts
@@ -3,10 +3,28 @@ import { END_POINTS } from "@/constants/endpoint";
 import { IBillResponse, IBillStatus } from "@/models/bill";
 import { cookies } from "next/headers";
 
-// https://api.devsphere.id.vn/api/v1/bills/admin/search?tripFrom=2024-11-15 00:00:00&tripTo=2024-11-20 23:59:59
-export const searchBill = async (current: number): Promise<IBillResponse> => {
+interface IBillFilterProps {
+   current: number;
+   phoneNumber?: number;
+   tripFrom?: string;
+}
+
+export const searchBill = async ({
+   current,
+   phoneNumber,
+}: IBillFilterProps): Promise<IBillResponse> => {
    const token = cookies().get("access_token")?.value;
-   const url = `${process.env.NEXT_PUBLIC_FUTA_API_URL}/${END_POINTS.BILL.SEARCH}?tripFrom=2024-11-15 00:00:00&tripTo=2024-11-30 23:59:59&page=${current}`;
+   const params = new URLSearchParams({
+      tripFrom: "2024-11-15 00:00:00",
+      tripTo: "2024-11-30 23:59:59",
+      page: current.toString(),
+      size: "10",
+      phoneNumber: phoneNumber ? phoneNumber.toString() : "",
+   });
+
+   const url = `${process.env.NEXT_PUBLIC_FUTA_API_URL}/${
+      END_POINTS.BILL.SEARCH
+   }?${params.toString()}`;
    const res = await fetch(url, {
       method: "GET",
       headers: {
@@ -14,9 +32,6 @@ export const searchBill = async (current: number): Promise<IBillResponse> => {
       },
    });
    const result = await res.json();
-   console.log("====================================");
-   console.log(result);
-   console.log("====================================");
    return result;
 };
 
